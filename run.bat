7f1a892c82237a0239e12acca239d245c621d8f5
melayudi2002-creator <melayudi2002@gmail.com>
2026-05-22 08:16:08 +0700

    Add files via upload

---
+++ run.bat
@@ -0,0 +1,16 @@
+@echo off
+title Menjalankan Bot (main.py)
+cls
+
+echo ==========================================
+echo Menjalankan skrip Python main.py...
+echo ==========================================
+echo.
+
+python run.py
+
+echo.
+echo ==========================================
+echo Skrip telah berhenti.
+echo ==========================================
+pause
